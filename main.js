712a1c9945c4acac6b1f2249b817c83331db059d
pontin1 <125661535+pontin1@users.noreply.github.com>
2023-11-29 14:33:28 -0300

    29/11

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-
-function tocaSomPom(){
- document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementAudio) {
+    document.querySelector(idElementAudio).play();
 }
+
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //01-11-2023- lista numerada
 let contador = 0;
 
-while(contador <listaDeTeclas.length){
+//enquanto
+while (contador < listaDeTeclas.length) {
+  
+  conaole.log(instrumento) 
+   
+    const instrumento = listaDeTeclas[contador].classList[1];
+    listaDeTeclas[contador].classList[1];
 
-    listaDeTeclas[0].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom('#som_tecla_pom')
+    }
     contador = contador + 1;
 
     console.log(contador);
